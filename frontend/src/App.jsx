ebadcca58fa0026fc77747de2bfa51af9a170f10
Aditya <adityajogdand15@gmail.com>
2025-05-29 15:23:18 +0530

    frontend app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,30 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import HomePage from "./pages/HomePage";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import Logout from './pages/Logout';
+import ProtectedRoute from './components/ProtectedRoute';
+import AddPatient from './pages/AddPatients';
 
-export default function App() {
+function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/home" element={<HomePage />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/add-patient" element={ <ProtectedRoute> <AddPatient /> </ProtectedRoute> }
+/>
       </Routes>
     </Router>
   );
 }
+
+export default App;
